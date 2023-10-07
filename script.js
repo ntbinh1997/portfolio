bea372f7b137c32ff993c2c835db1535e677a105
Nguyen Thanh Binh <ntbinh1271997@gmail.com>
2023-10-07 17:22:02 +0700

    init project

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.querySelector(".menu-link");
+    var icon = document.querySelector(".menu-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
